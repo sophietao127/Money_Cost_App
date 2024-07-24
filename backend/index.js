b2fbc81627c954f1f987fc3516eb67b05ddb6c4e
sophietao127 <sophietao127@gmail.com>
2024-07-24 12:16:38 -0700

    setup backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server);
+
+console.log(`Server ready at ${url}`);
