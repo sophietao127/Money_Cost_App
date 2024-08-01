cb553d962494023663d3bdc8cfb628548fd36f6b
sophietao127 <sophietao127@gmail.com>
2024-08-01 14:02:25 -0700

    connect with MongoDB

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    // console.log(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
+import { connectDB } from "./db/connectDB.js";
+
 dotenv.config();
 
 // Required logic for integrating with Express
@@ -42,5 +44,6 @@ app.use(
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000/`);
